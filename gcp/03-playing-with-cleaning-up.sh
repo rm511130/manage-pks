755c6c0ee7b1ef6744c1b3c54fb887389ae163b2
Ralph Meira <rmeira@pivotal.io>
2020-03-15 12:05:15 -0400

    Update 03-playing-with-cleaning-up.sh

--- gcp/03-playing-with-cleaning-up.sh
+++ gcp/03-playing-with-cleaning-up.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # How to automate the clean-up of multiple (5 in this example) PKS K8s Clusters on GCP
-# Cluste names: user1-cluster, userr2-cluster, ... user5-cluster
+# Cluste names: user1-cluster, user2-cluster, ... user5-cluster
 
-for i in {1..5}; do echo "user$i-cluster" | ./manage-cluster cleanup; done
+pks login -a https://api.pks.pks4u.com:9021 -u pks_admin -p password -k
+for i in {1..25}; do echo "user$i-cluster" | ./manage-cluster cleanup; done
