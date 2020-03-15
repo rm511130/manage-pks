3ed4de9c4ecc1d4ba5f105817dd06ae44cdb1664
Ralph Meira <rmeira@pivotal.io>
2020-03-15 12:05:58 -0400

    Update 04-playing-with-pks-delete-cluster.sh

--- gcp/04-playing-with-pks-delete-cluster.sh
+++ gcp/04-playing-with-pks-delete-cluster.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # How to automate PKS DELETE-CLUSTER of multiple (5 in this example) PKS K8s Clusters on GCP
-# Cluste names: user1-cluster, userr2-cluster, ... user5-cluster
+# Cluste names: user1-cluster, user2-cluster, ... user5-cluster
 
-for i in {1..5}; do pks delete-cluster "user$i-cluster" --non-interactive; done
+pks login -a https://api.pks.pks4u.com:9021 -u pks_admin -p password -k
+for i in {1..25}; do pks delete-cluster "user$i-cluster" --non-interactive; done
