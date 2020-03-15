816a81307a1a95eb87302b28e2bbfb9ddf0b8a24
Ralph Meira <rmeira@pivotal.io>
2020-03-15 12:04:27 -0400

    Update 02-playing-with-accessing.sh

--- gcp/02-playing-with-accessing.sh
+++ gcp/02-playing-with-accessing.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # How to automate the access to multiple PKS K8s Clusters
 # Cluster names: user1-cluster, user2-cluster, ... user5-cluster
+# Using differeny devops1, devops2, ... cluster owners with management scope
+# respectively
 
-for i in {1..5}; do echo "user$i-cluster" | ./manage-cluster access; done
+for i in {1..2}
+do 
+	pks login -a https://api.pks.pks4u.com:9021 -u devops$i -p password -k
+	echo "user$i-cluster" | ./manage-cluster access
+done
